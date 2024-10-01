ea292318490b686a34bb8b4f2f286e255b88d53e
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-01 14:58:16 +0200

    add the code to js script

--- assets/script.js
+++ assets/script.js
@@ -26,7 +26,15 @@ console.log("test");
 // else stamp the number
 
 // 3. Output
-
+for (let number = 1 ; number < 100 ; number++){
+    if (number % 3 == 0){
+        console.log('Fizz')
+    } else if (number % 5 == 0){
+        console.log('FizzBuzz')
+    } else {
+        console.log(number)
+    }
+}
 
 // Complete the assignment
 
