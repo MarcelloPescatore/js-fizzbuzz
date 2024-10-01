82841c14613d5d3cd9c3cf07bacd2c91656aeeb6
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-01 15:07:15 +0200

    find out an incorrect analysis of the assignment:
    - fix the analysis and the code

--- assets/script.js
+++ assets/script.js
@@ -20,19 +20,24 @@ console.log("test");
 // 2. Proceede to set up the code 
 
 // We need a program that stamp numbers from 1 to 100
-// Then we put 2 conditions:
-// 1. if the number is a multiply of 3, stamp "Fizz"
-// 2. else if the number is a multiply of 5, stamp "FizzBuzz"
+// Then we put 3 conditions:
+// 1. if the number is a multiply of 3 and 5, stamp "FizzBuzz"
+// 2. if the number is a multiply of 3, stamp "Fizz"
+// 3. else if the number is a multiply of 5, stamp "Buzz"
 // else stamp the number
 
 // 3. Output
-for (let number = 1 ; number < 100 ; number++){
-    if (number % 3 == 0){
-        console.log('Fizz')
-    } else if (number % 5 == 0){
-        console.log('FizzBuzz')
-    } else {
-        console.log(number)
+for (let number = 1 ; number <= 100 ; number++){
+    if (number % 3 == 0 && number % 5 == 0){
+        console.log('FizzBuzz');
+    } else{
+        if (number % 3 == 0){
+            console.log('Fizz')
+        } else if (number % 5 == 0){
+            console.log('Buzz')
+        } else {
+            console.log(number)
+        }
     }
 }
 
